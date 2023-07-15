e66bb9dc5a75656c788eb14c59ef4b6a3209a941
Zach <zacharyzwilson@outlook.com>
2023-07-14 19:28:40 -0500

    update

--- backend/db.js
+++ backend/db.js
@@ -6,16 +6,17 @@ const { getDatabaseUri } = require("./config");
 let db;
 
 if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+	db = new Client({
+		connectionString: getDatabaseUri(),
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
 } else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
+	db = new Client({
+		host: "/var/run/postgresql/",
+		database: getDatabaseUri(),
+	});
 }
 
 db.connect();
--- jobly/src/routes-nav/Routes.js
+++ jobly/src/routes-nav/Routes.js
@@ -9,74 +9,53 @@ import ProfileForm from "../profiles/ProfileForm";
 import SignupForm from "../auth/SignupForm";
 import PrivateRoute from "./PrivateRoute";
 
-/** Site-wide routes.
- *
- * Parts of site should only be visitable when logged in. Those routes are
- * wrapped by <PrivateRoute>, which is an authorization component.
- *
- * Visiting a non-existant route redirects to the homepage.
- */
-
 function AppRoutes({ login, signup }) {
-	// renamed function here
 	console.debug(
-		"AppRoutes", // and here
+		"AppRoutes",
 		`login=${typeof login}`,
-		`register=${typeof register}`
+		`signup=${typeof signup}`
 	);
 
 	return (
-		<div className="pt-5">
-			<Routes>
-				<Route path="/" element={<Homepage />} />
-
-				<Route path="/login" element={<LoginForm login={login} />} />
-
-				<Route
-					path="/signup"
-					element={<SignupForm signup={signup} />}
-				/>
-
-				<Route
-					path="/companies"
-					element={
-						<PrivateRoute>
-							<CompanyList />
-						</PrivateRoute>
-					}
-				/>
-
-				<Route
-					path="/jobs"
-					element={
-						<PrivateRoute>
-							<JobList />
-						</PrivateRoute>
-					}
-				/>
-
-				<Route
-					path="/companies/:handle"
-					element={
-						<PrivateRoute>
-							<CompanyDetail />
-						</PrivateRoute>
-					}
-				/>
-
-				<Route
-					path="/profile"
-					element={
-						<PrivateRoute>
-							<ProfileForm />
-						</PrivateRoute>
-					}
-				/>
-
-				<Route path="*" element={<Navigate to="/" />} />
-			</Routes>
-		</div>
+		<Routes>
+			<Route path="/" element={<Homepage />} />
+			<Route path="/login" element={<LoginForm login={login} />} />
+			<Route path="/signup" element={<SignupForm signup={signup} />} />
+			<Route
+				path="/companies"
+				element={
+					<PrivateRoute>
+						<CompanyList />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/jobs"
+				element={
+					<PrivateRoute>
+						<JobList />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/companies/:handle"
+				element={
+					<PrivateRoute>
+						<CompanyDetail />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/profile"
+				element={
+					<PrivateRoute>
+						<ProfileForm />
+					</PrivateRoute>
+				}
+			/>
+			<Route path="*" element={<Navigate to="/" />} />
+		</Routes>
 	);
 }
 
-export default AppRoutes; // and here
+export default AppRoutes;
